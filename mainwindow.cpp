9703b51dc89f42a83fa70677f34017d38a4166dc
Adrian <adrian@mxlinux.org>
2021-07-20 17:19:41 -0400

    Save config file under MX-Linux

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -69,7 +69,7 @@ MainWindow::MainWindow(const QCommandLineParser &arg_parser, QWidget *parent)
 
 MainWindow::~MainWindow()
 {
-    QSettings settings(qApp->applicationName());
+    QSettings settings(qApp->organizationName(), qApp->applicationName());
     settings.setValue("geometry", saveGeometry());
 }
 
@@ -123,7 +123,7 @@ void MainWindow::displaySite(QString url, QString title)
     int height = 500;
     this->resize(width, height);
 
-    QSettings settings(qApp->applicationName());
+    QSettings settings(qApp->organizationName(), qApp->applicationName());
     if (settings.contains("geometry")) {
         restoreGeometry(settings.value("geometry").toByteArray());
         if (this->isMaximized()) { // add option to resize if maximized
