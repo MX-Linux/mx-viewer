24766266683f8b4fae5af2632b9166fb05c77ff3
Adrian <adrian@mxlinux.org>
2026-01-14 20:16:41 -0500

    Add keyboard shortcuts for navigation

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1164,6 +1164,18 @@ void MainWindow::keyPressEvent(QKeyEvent *event)
         currentWebView()->setFocus();
     } else if (event->key() == Qt::Key_L && event->modifiers() == Qt::ControlModifier) {
         focusAddressBar();
+    } else if (event->key() == Qt::Key_R && event->modifiers() == Qt::ControlModifier) {
+        if (auto *view = currentWebView()) {
+            view->reload();
+        }
+    } else if (event->key() == Qt::Key_Left && event->modifiers() == Qt::AltModifier) {
+        if (auto *view = currentWebView()) {
+            view->back();
+        }
+    } else if (event->key() == Qt::Key_Right && event->modifiers() == Qt::AltModifier) {
+        if (auto *view = currentWebView()) {
+            view->forward();
+        }
     }
 }
 
